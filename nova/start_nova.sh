0daace43ba182ecda71a2ce6225e3d4933dadbe1
Tim Beck <tmbeck@gmail.com>
2022-09-11 11:30:55 -0700

    Switch to calling manage.py directly

--- nova/start_nova.sh
+++ nova/start_nova.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 echo "starting django wsgi app in gunicorn..."
-# python manage.py runserver 0.0.0.0:8000 &
-gunicorn net.wsgi:application -b 0.0.0.0:8000 &
+python manage.py runserver 0.0.0.0:8000 &
+#gunicorn net.wsgi:application -b 0.0.0.0:8000 &
+
 echo "starting solve processor..."
 python process_submissions.py --solve-locally=$(pwd)/solve_script.sh
